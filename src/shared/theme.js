3cc96bfff62d627768caea45b54a3e8f7b8a0dcf
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-12-10 13:34:34 +0900

    Add theme.js
    styled-component에서 제공하는 ThemeProvider를 통해서 전역으로 스타일을 적용시켜 재사용성을 높일려고 함

---
+++ src/shared/theme.js
@@ -0,0 +1,20 @@
+const size = {
+  mobile: "375px",
+  tablet: "768px",
+  laptop: "1024px",
+};
+
+const theme = {
+  button1Color: "#8BAEEF",
+  button2Color: "#F9A7A7",
+  titleColor: "#92A8D1",
+  inputColor: "#666666",
+  whiteColor: "#ffffff",
+  contentColor: "#FDFDFD",
+  boxShadow: "0px 4px 15px rgba(194, 194, 194, 0.4)",
+  mobile: `(max-width: ${size.mobile})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(max-width: ${size.laptop})`,
+};
+
+export default theme;
